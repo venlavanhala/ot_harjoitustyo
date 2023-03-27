a6fa34df7afce4beed6af6e339c6accd702d24ba
Venla Vanhala <venla.e.vanhala@gmail.com>
2023-03-27 10:32:07 +0300

    lisätty poetry ominaisuuksia

---
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -0,0 +1,13 @@
+import unittest
+from maksukortti import Maksukortti
+
+class TestMaksukortti(unittest.TestCase):
+    def setUp(self):
+        print("Set up goes here")
+
+    def test_konstruktori_asettaa_saldon_oikein(self):
+        # alustetaan maksukortti, jossa on 10 euroa (1000 senttiä)
+        kortti = Maksukortti(1000)
+        vastaus = str(kortti)
+
+        self.assertEqual(vastaus, "Kortilla on rahaa 10.00 euroa")
