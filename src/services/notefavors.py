937757eee37cdab132d7eade2b90188dea6676a3
Venla Vanhala <venla.e.vanhala@gmail.com>
2023-04-10 17:28:02 +0300

    lisätty notefavors rekisteröinti

--- src/services/notefavors.py
+++ src/services/notefavors.py
@@ -8,5 +8,10 @@ from repositories.user_repository import UserTools
 class NoteFavors:
     def __init__(self, note_repository, user_repository):
         self._user = None
-        self._todo_repository = todo_repository
+        self._note_repository = note_repository
         self._user_repository = user_repository
+
+    def sign_up(self, name, password):
+        #tarkista ettei ole jo tätä käyttäjänimeä
+        create=self._user_repository.new_user(User(name, password))
+        return create
