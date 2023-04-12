c708d2e50a06a3a8463c7f44615abd5ca7e924e1
Venla Vanhala <venla.e.vanhala@gmail.com>
2023-04-12 22:44:18 +0300

    muokattu kirjautumisnäkymää

--- src/screen/sign_in.py
+++ src/screen/sign_in.py
@@ -1,6 +1,31 @@
-from tkinter import ttk, StringVar, constants
+from tkinter import ttk, StringVar, constants, Tk
 from services.notefavors import notefavors
 
 class SignInScreen:
-    def __init__(self):
+    def __init__(self, root):
+        self._root=root
+        self._name=""
+        self._password=""
+
+    def view(self):
+        label=ttk.Label(master=self._root, text="Kirjaudu sisään")
+        self._name=ttk.Entry(master=self._root, text="Käyttäjänimi")
+        self._password=ttk.Entry(master=self._root, text="Salasana")
+        signing=ttk.Button(master=self._root, text="Rekisteröidy")
+
+        label.pack()
+        self._name.pack()
+        self._password.pack()
+        signing.pack()
+
+    def check(self):
         pass
+        #check if the name and password exist
+
+window = Tk()
+window.title("Kirjautuminen")
+
+ui = SignInScreen(window)
+ui.start()
+
+window.mainloop()
