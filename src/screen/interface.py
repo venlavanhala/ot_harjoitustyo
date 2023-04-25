310f832dade58352220606c1b3ca6ba114537770
Venla Vanhala <venla.e.vanhala@gmail.com>
2023-04-25 15:07:35 +0300

    yritän poistaa coveragen gitistä

--- src/screen/interface.py
+++ src/screen/interface.py
@@ -14,7 +14,6 @@ class Interface:
     def remove_view(self):
         if self._view:
             self._view.remove_screen()
-
         self._view = None
 
     def sign_view(self):
@@ -24,12 +23,12 @@ class Interface:
 
     def note_view(self):
         self.remove_view()
-        self._view=NoteScreen(self._root, self.sign_view)
+        self._view=NoteScreen(self._root, self.sign_view, self.sign_view)
         self._view.pack()
 
     def signup_view(self):
         self.remove_view()
-        self._view=SignUpScreen(self._root, self.sign_view)
+        self._view=SignUpScreen(self._root, self.note_view)
         self._view.pack()
 
 
--- src/screen/noteview.py
+++ src/screen/noteview.py
@@ -2,14 +2,13 @@ from tkinter import ttk, constants
 from services.notefavors import NoteFavors
 
 class NoteScreen:
-    def __init__(self, root, notes, logout):
+    def __init__(self, root, logout):
         self._root=root
-        self._notes=notes
+        self._notes=None
         self._frame=None
-        self._user=NoteFavors.current_user()
+        self._user=NoteFavors.current_user
         self.logout=logout
         self.newnote=None
-
         self.format()
 
     def remove_screen(self):
@@ -18,10 +17,9 @@ class NoteScreen:
     def all_notes(self):
         self._notes=NoteFavors.return_notes(self._user)
 
-    def new_note(self, content):
+    def new_note(self):
         content=self.newnote.get()
         NoteFavors.new_note(content)
-
 
     def pack(self):
         self._frame.pack(fill=constants.X)
@@ -29,11 +27,12 @@ class NoteScreen:
     def format(self):
         self.all_notes
         header=ttk.Label(master=self._root, text="Muistiinpanosi")
+        self.all_notes
         for note in self._notes:
             content=ttk.Label(master=self._root, text=note.text)
             content.pack()
         new=ttk.Label(master=self._root, text="Uusi muistiinpano")
-        self.newnote=ttk.Entry(master=self._root, text="teksti: ")
+        self.newnote=ttk.Entry(master=self._root, text="teksti: ", command=self.new_note)
         out=ttk.Button(master=self._root, text="Kirjaudu ulos", command=self.logout)
         new.pack()
         header.pack()
