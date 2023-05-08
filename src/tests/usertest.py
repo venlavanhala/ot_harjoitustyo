a58db7cdcea50d4f9055e02bc42ef9e4eaaf486f
Venla Vanhala <venla.e.vanhala@gmail.com>
2023-05-08 19:54:59 +0300

    lisätty usertest

---
+++ src/tests/usertest.py
@@ -0,0 +1,30 @@
+import unittest
+from repositories.user_repository import user_repository
+from entities.user import User
+
+class TestUserRepository(unittest.TestCase):
+    def setUp(self):
+        user_repository.remove_users()
+        self.liisa = User(0,"Liisa", "kisu")
+        self.kerttu = User(0, "Kerttu", "koira")
+
+    def test_new_user(self):
+        user_repository.new_user(self.liisa)
+        users = user_repository.all_users()
+
+        self.assertEqual(len(users), 1)
+        self.assertEqual(users[0].name, self.liisa.name)
+
+    def test_all_users(self):
+        user_repository.new_user(self.liisa)
+        user_repository.new_user(self.kerttu)
+        users = user_repository.all_users()
+
+        self.assertEqual(len(users), 2)
+
+    def test_find_by_username(self):
+        user_repository.new_user(self.liisa)
+
+        user = user_repository.find_user(self.liisa.name, self.liisa.password)
+
+        self.assertEqual(user.name, self.liisa.name)
