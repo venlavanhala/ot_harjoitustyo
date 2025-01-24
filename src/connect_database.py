f69645e3479d11ef380390e30bf727fc32529f1d
venlavanhala <venla.e.vanhala@gmail.com>
2025-01-24 22:20:50 +0200

    remove an useless database file and check if connection works

--- src/connect_database.py
+++ src/connect_database.py
@@ -4,12 +4,22 @@ import os
 
 dirname=os.path.dirname(__file__)
 
-basename=os.getenv("basename") or "muistio.db"
-path=os.path.join(dirname, "..", basename)
+#basename=os.getenv("basename") or "muistio.db"
+path=os.path.join(dirname, "muistio.db")
 database=sqlite3.connect(path)
 
 database.row_factory = sqlite3.Row
 
 
+def chk_conn(conn): #check the connection
+     try:
+        conn.cursor()
+        return True
+     except Exception as ex:
+        return False
+
+
 def get_database_connection():
+    print(chk_conn(database))
     return database
+
